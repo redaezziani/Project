d7d0f8a8fb8d8e9f72e70d5edfb53eca4e662fc2
Catraco <gpx.zero.000@gmail.com>
2023-09-11 22:19:36 +0100

    Add data

--- help/src/App.jsx
+++ help/src/App.jsx
@@ -1,37 +1,16 @@
+// Main page //
+
 import { Route , Routes  } from 'react-router-dom'
-import Card from './components/Card'
-
-
 import Home from './pages/home'
 import NotFound from './pages/notFound'
-
 import Cities from './sections/cities'
 import Associations from './sections/associations'
 import Communication from './sections/communication'
 
-
-// testing data //
-const cardData = [
-  {
-    name: "  الرياض",
-    status: "منكوبة",
-    count: 12,
-    numberD:1000,
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/View_of_Aroumd.jpg/1024px-View_of_Aroumd.jpg",
-  },
-  {
-    name: "  الرياض",
-    status: "مساعدة",
-    count: 20,
-    numberD:100,
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrmI2fj0E3rE7yZk-Z10j8sa3I03voi0wp5A&usqp=CAU",
-  },
-];
-
 function App() {
 
   return (
-    <div className="w-full flex-col relative justify-center items-center flex p-3 overflow-hidden">
+    <div className="w-full relative flex flex-col justify-center items-center overflow-hidden">
       
       <Routes>
       <Route path="/" element={<Home/>}/>
--- help/src/pages/home.jsx
+++ help/src/pages/home.jsx
@@ -1,4 +1,25 @@
+// Home page //
+
 import NavBar from '../components/NavBar'
+import Card from '../components/Card';
+
+// Testing data //
+const cardData = [
+    {
+        name: "  الرياض",
+        status: "منكوبة",
+        count: 12,
+        numberD: 1000,
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/View_of_Aroumd.jpg/1024px-View_of_Aroumd.jpg",
+    },
+    {
+        name: "  الرياض",
+        status: "مساعدة",
+        count: 20,
+        numberD: 100,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrmI2fj0E3rE7yZk-Z10j8sa3I03voi0wp5A&usqp=CAU",
+    },
+];
 
 const Home = () => {
 
@@ -6,7 +27,6 @@ const Home = () => {
         <>
             <NavBar />
             <div className="section">
-                Default, landing page.
             </div>
         </>
     )
