ac996e599372e7b2d614e7e644649e9c9c74e559
redaezziani <2003100500167@ofppt-edu.ma>
2023-09-11 21:35:51 +0200

    New Card

--- help/src/components/Card.jsx
+++ help/src/components/Card.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 function Card({ name, count, imageUrl, numberD , status}) {
   return (
-    <div className="w-full relative bg-white shadow-md font-Cairo rtl-card flex-col  rounded-md justify-center items-center flex p-3 overflow-hidden col-span-1">
+    <div className="w-full relative bg-gray-300 shadow-md font-Cairo rtl-card flex-col  rounded-md justify-center items-center flex p-3 overflow-hidden col-span-1">
       <div className="img-container relative w-full h-40 rounded-md overflow-hidden">
-      <div className={`chip flex justify-center rounded-md absolute left-1 top-1 bg-opacity-80 items-center px-4 py-1 ${status=='منكوبة'?'bg-red-600':' bg-green-500'} text-white`}>
+      <div className={`chip flex justify-center rounded-md absolute left-1 top-1 items-center px-4 py-1 ${status=='منكوبة'?'bg-red-600':' bg-green-500'} text-white`}>
         <p className="text-xs font-semibold">
           {status}
         </p>
@@ -12,7 +12,7 @@ function Card({ name, count, imageUrl, numberD , status}) {
         <img src={imageUrl} alt="card" className="w-full h-full object-cover object-center" />
       </div>
 
-      <div className="card-info w-full">
+      <div className="card-info mt-3 w-full">
         <div className="name">
           <h3 className=" font-semibold ">
             {name}
