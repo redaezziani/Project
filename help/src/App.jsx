e5190eb4297cf5bfcf1babe091256514e328c04b
redaezziani <2003100500167@ofppt-edu.ma>
2023-09-11 21:28:58 +0200

    new card test

--- help/src/App.jsx
+++ help/src/App.jsx
@@ -5,10 +5,18 @@ import Card from './components/Card'; // Import the Card component without curly
 function App() {
   const cardData = [
     {
-      name: "مراجعة تجربة المستخدم/واجهة المستخدم",
+      name: "  الرياض",
+      status: "منكوبة",
       count: 12,
       numberD:1000,
       imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/View_of_Aroumd.jpg/1024px-View_of_Aroumd.jpg",
+    },
+    {
+      name: "  الرياض",
+      status: "مساعدة",
+      count: 20,
+      numberD:100,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrmI2fj0E3rE7yZk-Z10j8sa3I03voi0wp5A&usqp=CAU",
     },
   ];
   
--- help/src/components/Card.jsx
+++ help/src/components/Card.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Card({ name, count, imageUrl, numberD }) {
+function Card({ name, count, imageUrl, numberD , status}) {
   return (
-    <div className="w-full font-Cairo rtl-card flex-col relative rounded-md justify-center items-center flex p-3 overflow-hidden col-span-1">
-      <div className="img-container w-full rounded-md overflow-hidden">
+    <div className="w-full relative bg-white shadow-md font-Cairo rtl-card flex-col  rounded-md justify-center items-center flex p-3 overflow-hidden col-span-1">
+      <div className="img-container w-full h-40 rounded-md overflow-hidden">
         <img src={imageUrl} alt="card" className="w-full h-full object-cover object-center" />
       </div>
-      <div className="card-info">
+      <div className={`chip flex justify-center rounded-md absolute left-1 top-1 bg-opacity-80 items-center px-4 py-1 ${status=='منكوبة'?'bg-red-600':'bg-blue-600'} text-white`}>
+        <p className="text-xs font-semibold">
+          {status}
+        </p>
+      </div>
+      <div className="card-info w-full">
         <div className="name">
           <h3 className=" font-semibold ">
             {name}
           </h3>
         </div>
-        <div className="card-info flex justify-start items-start gap-1 flex-col">
+        <div className="card-info text-gray-500 w-full flex justify-start items-start gap-2 flex-col">
           <p>
             {count}
           </p>
@@ -21,7 +25,7 @@ function Card({ name, count, imageUrl, numberD }) {
             {numberD}
           </p>
         </div>
-        <Link to="/vile" className="flex text-slat items-center">
+        <Link to="/vile" className="flex cursor-pointer text-gray-600 mt-2 text-slat items-center">
             اقرأ المزيد
         </Link>
       </div>
